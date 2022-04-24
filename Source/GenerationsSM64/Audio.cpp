c3d196677e4131319cd865f7faa2dcbba9bf6888
GenerationsSM64 <102590186+GenerationsSM64@users.noreply.github.com>
2022-04-24 17:13:33 +0300

    Use the audio device set in configuration tool.

--- Source/GenerationsSM64/Audio.cpp
+++ Source/GenerationsSM64/Audio.cpp
@@ -1,8 +1,14 @@
 #include "Mod.h"
 
+#define MA_ENABLE_ONLY_SPECIFIC_BACKENDS
+#define MA_ENABLE_DSOUND
+
 #define MINIAUDIO_IMPLEMENTATION
 #include <miniaudio.h>
 
+constexpr ma_backend backends[] = { ma_backend_dsound };
+
+ma_context context;
 ma_device device;
 
 void audioCallback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount)
@@ -16,16 +22,11 @@ void audioCallback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uin
     (void)pInput;
 }
 
-void initAudio()
+HOOK(bool, __fastcall, LoadAudioConfig, 0xA5CAF0, struct AudioConfig* audioConfig)
 {
-    gSoundDataADSR = rom.get() + 0x57B720;
-    gSoundDataRaw = rom.get() + 0x593560;
-    gMusicData = rom.get() + 0x7B0860;
-    gBankSetsData = rom.get() + 0x7CC620;
-
-    audio_init();
-    sound_init();
-    sound_reset(0);
+    const bool result = originalLoadAudioConfig(audioConfig);
+    if (!result)
+        return false;
 
     ma_device_config deviceConfig = ma_device_config_init(ma_device_type_playback);
     deviceConfig.sampleRate = 32000;
@@ -34,7 +35,25 @@ void initAudio()
     deviceConfig.dataCallback = audioCallback;
     deviceConfig.playback.format = ma_format_s16;
     deviceConfig.playback.channels = 2;
+    deviceConfig.playback.pDeviceID = (const ma_device_id*)audioConfig;
 
-    ma_device_init(nullptr, &deviceConfig, &device);
+    ma_context_init(backends, _countof(backends), nullptr, &context);
+    ma_device_init(&context, &deviceConfig, &device);
     ma_device_start(&device);
+
+    return true;
+}
+
+void initAudio()
+{
+    INSTALL_HOOK(LoadAudioConfig);
+
+    gSoundDataADSR = rom.get() + 0x57B720;
+    gSoundDataRaw = rom.get() + 0x593560;
+    gMusicData = rom.get() + 0x7B0860;
+    gBankSetsData = rom.get() + 0x7CC620;
+
+    audio_init();
+    sound_init();
+    sound_reset(0);
 }
